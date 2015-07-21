a38aa018c569627d0aac0c703def19c46bb1bdce
Park June Chul <pjc0247@naver.com>
2015-07-21 17:43:36 +0900

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -5,8 +5,45 @@
 
 #include <chrono>
 #include <type_traits>
+#include <functional>
+
+#include <Windows.h>
 
 using namespace std;
+
+
+class foo {
+public:
+	foo() {}
+	foo(int n) : n(n) {}
+	foo(const foo &f){
+		printf("copy\n");
+
+		n = f.n;
+	}
+	foo(foo &&f) {
+		printf("move\n");
+
+		n = f.n;
+	}
+
+	foo *operator =(const foo &f){
+		printf("copy =\n");
+
+		n = f.n;
+
+		return nullptr;
+	}
+	foo *operator =(foo &&f) {
+		printf("move =\n");
+
+		n = f.n;
+
+		return nullptr;
+	}
+
+	int n;
+};
 
 #define await ~
 
@@ -48,7 +85,10 @@ _await(iawaitable<T> &&o) {
 
 template <typename T>
 void yield(T &&o){
-	printf("yield\n");
+	void *ptr = GetFiberData();
+	auto c = (coroutine<std::remove_reference_t<T>>*)ptr;
+
+	c->yield(std::forward<remove_reference_t<T>>(o));
 }
 
 class unit : public iawaitable<void>{
@@ -65,16 +105,144 @@ private:
 	std::chrono::milliseconds ms;
 };
 
+template <typename T>
+class task : iawaitable<T>{
+public:
+
+	virtual T operator~(){
+		return T();
+	}
+};
+
+class tasks{
+public:
+};
+
+class icoroutine{
+	virtual void yield() =0;
+};
+
+template <typename T>
+class coroutine{
+public:
+	class iterator{
+	public:
+		iterator(coroutine<T> &parent) :
+			parent(parent){
+		}
+
+		bool operator != (const iterator &other) const{
+			return !parent.eof;
+		}
+		T operator* (){
+			return std::move(buffer);
+		}
+		iterator& operator++ ()
+		{
+			parent.next(&buffer);
+
+			return *this;
+		}
+
+	private:
+		T buffer;
+		coroutine<T> &parent;
+	};
+
+public:
+	coroutine() :
+		this_fiber_ptr(nullptr), prev_fiber_ptr(nullptr){
+	}
+	coroutine(std::function<void()> &f) :
+		eof(false),
+		this_fiber_ptr(nullptr), prev_fiber_ptr(nullptr),
+		func(f){
+
+		init_fiber();
+	}
+	coroutine(std::function<void()> &&f) :
+		eof(false),
+		this_fiber_ptr(nullptr), prev_fiber_ptr(nullptr),
+		func(std::move(f)) {
+
+		init_fiber();
+	}
+
+	bool next(T *dst){
+		prev_fiber_ptr = GetCurrentFiber();
+		SwitchToFiber(this_fiber_ptr);
+
+		if(eof){
+			return false;
+		}
+		else{
+			*dst = std::move(yield_storage);
+			return true;
+		}
+	}
+
+	void yield(){
+		SwitchToFiber(prev_fiber_ptr);
+	}
+	void yield(T &&o){
+		yield_storage = std::forward<T>(o);
+
+		yield();
+	}
+
+	iterator begin(){
+		return std::move(++iterator(*this));
+	}
+	iterator end(){
+		return iterator(*this);
+	}
+
+private:
+	void init_fiber(){
+		this_fiber_ptr = CreateFiber(0, coroutine<T>::co_func, this);
+	}
+
+	static void __stdcall co_func(void *arg){
+		coroutine<T> *_this = (coroutine<T>*)arg;
+
+		_this->func();
+		_this->eof = true;
+		_this->yield();
+	}
+
+private:
+	T yield_storage;
+	std::function<void()> func;
+	void *this_fiber_ptr;
+	void *prev_fiber_ptr;
+
+	bool eof;
+};
+
+class coroutines{
+public:
+	template <typename T>
+	static coroutine<T> create(std::function<void()> &f){
+		return coroutine<T>(f);
+	}
+	template <typename T>
+	static coroutine<T> create(std::function<void()> &&f) {
+		return coroutine<T>(std::forward<std::function<void()>>(f));
+	}
+};
 
 int _tmain(int argc, _TCHAR* argv[]){
+	ConvertThreadToFiber(nullptr);
 
-	auto v = unit(1ms);
+	auto a = coroutines::create<foo>([](){
+		for(int i=0;i<5;i++){
+			yield(i);
+		}
+	});
 
-	await v;
-
-	auto a = 1ms;
-
-	unit(1ms);
+	for(foo v : a){
+		printf("%d\n", v.n);
+	}
 
 	return 0;
 }
